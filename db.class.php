ebb3309f7291985b0c53ed0a866cee6efe30df89
Rainan Gramacho <rai_bomfim@hotmail.com>
2018-12-04 14:46:10 -0300

    Rede social para músicos

---
+++ db.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class db {
+
+	//host
+	//private $host = 'localhost';
+	private $host = 'localhost';
+
+	//usuario
+	//private $usuario = 'id7172651_rainan';
+	private $usuario = 'root';
+
+	//senha
+	//private $senha = 'rainan';
+	private $senha = '';
+
+	//banco de dados
+	//private $database = 'id7172651_twitter_clone2';
+	private $database = 'twitter_clone';
+
+	public function conecta_mysql(){
+
+		//criar a conexao
+		$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+
+		//ajustar o charset de comunicação entre a aplicação e o banco de dados
+		mysqli_set_charset($con, 'utf8');
+
+		//verficar se houve erro de conexão
+		if(mysqli_connect_errno()){
+			echo 'Erro ao tentar se conectar com o BD MySQL: '.mysqli_connect_error();	
+		}
+
+		return $con;
+	}
+
+}
+
+?>
---
+++ pagina_voltar.php
@@ -0,0 +1,82 @@
+
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<title>Our Song</title>
+		<link rel = "shortcut icon" type = "imagem/x-icon" href = "imagens/icone_twitter.png"/>
+		<!-- jquery - link cdn -->
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+		<!-- bootstrap - link cdn -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<style>
+	button{
+     width: 35%;
+		 height:40px;
+	}
+	.jumbotron{
+       text-align: center;
+	}
+
+	</style>
+		
+	</head>
+
+	<body>
+
+		
+	        
+		<!-- Static navbar -->
+	    <nav class="navbar navbar-default navbar-static-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	           <a href="index.php">
+	          <img src="imagens/icone_twitter.png" style="width:10%"/>
+			  </a>
+	        </div>
+	        
+	        <div id="navbar" class="navbar-collapse collapse">
+						<div class="col-md-12">
+				    		
+			
+						</form>
+				  	</ul>
+	            </li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+
+
+
+	    <div class="container">
+
+	      <!-- Main component for a primary marketing message or call to action -->
+	      <div class="jumbotron" >
+	        <h1>Our Song</h1>
+					<p>Yayy!!! Você está cadastrado, agora é só fazer login!</p>
+					<a href="index.php">
+						<button type="buttom" class="btn btn-primary">Come On!</button>
+						
+					</a>
+	      </div>
+
+	      <div class="clearfix"></div>
+		</div>
+
+
+	    </div>
+	
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
+	</body>
+</html>
